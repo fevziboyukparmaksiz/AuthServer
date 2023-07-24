ac93ee439d2170094ed16edf760abd99907a3870
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-07-24 17:16:36 +0300

    Created ProductController

---
+++ UdemyAuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyAuthServer.Core.DTOs;
+using UdemyAuthServer.Core.Models;
+using UdemyAuthServer.Core.Services;
+
+namespace UdemyAuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IGenericService<Product, ProductDto> _productService;
+
+        public ProductController(IGenericService<Product, ProductDto> productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return ActionResultInstance(await _productService.GetAllByAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Update(productDto, productDto.Id));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            return ActionResultInstance(await _productService.Remove(id));
+        }
+    }
+}
