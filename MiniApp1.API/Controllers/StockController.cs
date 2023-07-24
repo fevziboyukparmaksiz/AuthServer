93f4502d9bcb25ab81fda82ba4b32332118bfc93
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-07-24 19:35:09 +0300

    Create Controller for Mini API

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name;
+
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            //StockId StockQuantity Category UserId/UserName
+            return Ok($"Stock İşlemleri => UserName: {userName}- UserId: {userId}");
+
+        }
+    }
+}
