7306c1bd750bdfac2e6d66c4201cdf9ffd4155ac
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-07-24 15:10:36 +0300

    Program.cs Configuration -2

--- UdemyAuthServer.API/Program.cs
+++ UdemyAuthServer.API/Program.cs
@@ -10,6 +10,7 @@ using UdemyAuthServer.Data;
 using Microsoft.EntityFrameworkCore;
 using UdemyAuthServer.Core.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,9 +44,40 @@ builder.Services.AddIdentity<UserApp, IdentityRole>(setupAction: opt =>
 }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
 builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+
 builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
 
+builder.Services.AddAuthentication(configureOptions: options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, configureOptions: options =>
+{
+    var tokenOptions = builder.Configuration.GetSection("TokenOption").Get<CustomTokenOption>();
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidIssuer = tokenOptions.Issuer,
+        ValidAudience = tokenOptions.Audience[0],
+        IssuerSigningKey = SignService.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+
+    };
+
+});
+
+
+
+
+
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
